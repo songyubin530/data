a80171ab4716ff6e499f713a60729a305ab3e8e8
songyubin530 <31025533+songyubin530@users.noreply.github.com>
2017-08-19 11:48:06 +0800

    Add files via upload

---
+++ java组数据/coolweather/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by X230 on 2017/8/16.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ java组数据/coolweather/java/com/coolweather/android/gson/Weather.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by X230 on 2017/8/16.
+ */
+
+public class Weather {
+    public String status;
+    public Basic basic;
+    public AQI aqi;
+    public Now now;
+    public Suggestion suggestion;
+    @SerializedName("daily_forecast")
+    public List<Forecast> forecastList;
+}
